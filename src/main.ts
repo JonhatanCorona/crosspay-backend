709dafa060497fa3d81c5a1aab427ddb205922e5
Jonhatan Corona <coronajonhatan@gmail.com>
2025-09-28 21:22:28 -0400

    se agrega env.example

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.APP_PORT ?? 3000);
+  await app.listen(process.env.APP_PORT ?? 3001);
 }
 bootstrap();
